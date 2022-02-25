c9c572559a76f897659f468445a075d273b66888
MiguelPimentel97 <miguel.vicente.calado@gmail.com>
2022-02-25 13:00:15 +0000

    Finished Compex Queries

--- bigquery_functions.py
+++ bigquery_functions.py
@@ -1,5 +1,6 @@
 # Import Packages
 from google.cloud import bigquery
+from time import time
 import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sql-bigquery-341310-c5b9409e541c.json"
@@ -36,3 +37,16 @@ def run_query(query, client, max_bytes):
     query_job = client.query(query, job_config=safe_config)
     
     return query_job.to_dataframe()
+
+def show_amount_of_data_scanned(client, query):
+    # dry_run lets us see how much data the query uses without running it
+    dry_run_config = bigquery.QueryJobConfig(dry_run=True)
+    query_job = client.query(query, job_config=dry_run_config)
+    print('Data processed: {} GB'.format(round(query_job.total_bytes_processed / 10**9, 3)))
+    
+def show_time_to_run(client, query):
+    time_config = bigquery.QueryJobConfig(use_query_cache=False)
+    start = time()
+    query_result = client.query(query, job_config=time_config).result()
+    end = time()
+    print('Time to run: {} seconds'.format(round(end-start, 3)))
